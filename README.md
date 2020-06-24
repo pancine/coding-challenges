97c2fbd0ce30b83a201e6380d8a1171147f7c8f7
Pedro <pedrolucaspancine@gmail.com>
2020-06-24 17:32:29 -0300

    att attREADME.md

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Here you will find my solves for a lot of problems in URI
 
 ## Stats
-* Python = 22.73%
-* C = 68.18%
-* SQL = 9.09%
+* Python = 21.13%
+* C = 73.24%
+* SQL = 5.63%
--- attREADME.py
+++ attREADME.py
@@ -1,10 +1,16 @@
-Python = 5
-C = 15
-SQL = 2
+Python = 15
+C = 52
+SQL = 4
 Total = Python + C + SQL
 
 pfile = open("README.md", "r")
 list = pfile.readlines()
+
+"""
+O cálculo é feito baseado no peso(nível das questões) dividido pelo 
+total de pontos.
+Ex: Uma questão de nível 9 + uma de nível 5 geram 14 pontos no total.
+"""
 
 list[4] = "* Python = %.2f%c\n"%(Python*100/Total, "%")
 list[5] = "* C = %.2f%c\n"%(C*100/Total,"%")
