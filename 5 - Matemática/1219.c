1986ce594e3b933e7d38865a17761673842b9aef
Pedro Lucas Pancine Chaves <pedrolucaspancine@gmail.com>
2020-09-20 19:56:52 -0300

    1219.c

---
+++ 5 - Matemática/1219.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int a, b, c;
+	double area, s, r, R, PI = acos(-1);
+	while (scanf("%d %d %d", &a, &b, &c) != EOF) {
+		s = (a+b+c) / 2.0;
+		area = sqrt(s * (s-a) * (s-b) * (s-c));
+		r = area / s;
+		R = (a*b*c) / (4.0 * area);
+		printf("%.4lf %.4lf %.4lf\n", (PI*R*R)-area, area-(PI*r*r), PI*r*r);
+	}
+	return 0;
+}
