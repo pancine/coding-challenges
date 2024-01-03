eba3261c5e8ac2981b6df12426a17333ef4bb899
Pedro Pancine <pedro.pacine@mindgeekconsultants.com>
2024-01-03 10:37:04 -0300

    CPP BASE

--- cpp_base.cpp
+++ cpp_base.cpp
@@ -8,11 +8,15 @@ using namespace std;
 #define cti cin.tie(NULL);
 #define endl "\n"
 
-
-int main() { 
-    cti; ibs;
+int main()
+{
+    cti;
+    ibs;
     int test_cases;
     cin >> test_cases;
 
     return 0;
 }
+
+#define coutMatrix(matrix) for(size_t i=0;i<matrix.size();i++){for(size_t j=0;j<matrix[i].size();j++){cout<<matrix[i][j]<<" ";}cout<<"\n";}
+#define coutVector(vector) for(size_t i=0;i<vector.size();i++){cout<<vector[i]<<" ";}cout<<"\n";
